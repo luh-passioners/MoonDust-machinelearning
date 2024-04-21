504dd190b8f67d8a3692575d4f30b85ae662037d
Marvin Lin <81843689+larvinmin@users.noreply.github.com>
2024-04-21 01:29:25 -0400

    minor improvements

--- Fraud_Detection/Detection.py
+++ Fraud_Detection/Detection.py
@@ -25,7 +25,7 @@ X = data[["Transaction id", "amount", "merchant id", "sector in company", "day i
 X_train, X_test = train_test_split(X, test_size=0.2, random_state=42)
 
 # Define and train Isolation Forest model
-model = IsolationForest(n_estimators=100, max_samples='auto', contamination=0.01, random_state=42)
+model = IsolationForest(n_estimators=200, max_samples='auto', contamination=0.03, random_state=42)
 model.fit(X_train)
 
 # Predict outliers on testing data
@@ -34,5 +34,34 @@ anomaly_scores = model.decision_function(X_test)
 
 # Analyze results: y_predicted will contain -1 for anomalies and 1 for normal transactions
 # anomaly_scores can be used to further analyze potential fraud based on score
+
+
+
+
+
+
+
+
+#new data
+
+
+new_transaction = pd.read_csv("new_transaction_data.csv")
+new_transaction["amount"] = new_transaction["amount"].str.replace("$", "").astype(float)
+
+# Create LabelEncoder objects for each categorical column
+le_merchant_id = LabelEncoder()
+le_sector = LabelEncoder()
+
+# Encode the categorical columns
+new_transaction["merchant id"] = le_merchant_id.fit_transform(new_transaction["merchant id"])
+new_transaction["sector in company"] = le_sector.fit_transform(new_transaction["sector in company"])
+
+new_transaction_encoded = new_transaction[["Transaction id", "amount", "merchant id", "sector in company", "day in month"]]
+
+new_prediction = model.predict(new_transaction_encoded)
+new_score = model.decision_function(new_transaction_encoded)
+print(new_score)
+
+
 
 
