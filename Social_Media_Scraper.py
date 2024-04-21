e0f43e4662022fa1a81007094d6ceee13f847eb5
Marvin Lin <81843689+larvinmin@users.noreply.github.com>
2024-04-21 00:22:11 -0400

    fraud detection training

--- Social_Media_Scraper.py
+++ Social_Media_Scraper.py
@@ -7,7 +7,6 @@ from SentimentAnalyze import analyze
 def scrape_for_board(ticker):
         url_Start = "https://investorshub.advfn.com/v2/boards?query="
         url = url_Start + ticker
-        print(url)
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         response = requests.get(url, headers = headers)
         if response.status_code == 200:
